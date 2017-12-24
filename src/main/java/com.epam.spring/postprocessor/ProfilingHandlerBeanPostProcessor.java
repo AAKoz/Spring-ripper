22c2e26e3638243303a9703de35e6985912ee5fe
Aleksandr Kozlov <Aleksandr_Kozlov2@epam.com>
2017-12-25 02:08:33 +0300

    Profiling added

--- src/main/java/com.epam.spring/postprocessor/ProfilingHandlerBeanPostProcessor.java
+++ src/main/java/com.epam.spring/postprocessor/ProfilingHandlerBeanPostProcessor.java
@@ -6,6 +6,9 @@ import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.lang.Nullable;
 
+import javax.management.MBeanServer;
+import javax.management.ObjectName;
+import java.lang.management.ManagementFactory;
 import java.lang.reflect.Proxy;
 import java.util.HashMap;
 import java.util.Map;
@@ -22,6 +25,11 @@ public class ProfilingHandlerBeanPostProcessor implements BeanPostProcessor {
             map.put(beanName, beanClass);
         }
         return bean;
+    }
+
+    public ProfilingHandlerBeanPostProcessor() throws Exception {
+        MBeanServer platformMBeanServer = ManagementFactory.getPlatformMBeanServer();
+        platformMBeanServer.registerMBean(controller, new ObjectName("profiling", "name", "controller"));
     }
 
     @Nullable
